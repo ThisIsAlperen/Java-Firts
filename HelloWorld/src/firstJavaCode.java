fc91e0eda839c88e96850e6cf746a9207630bfd3
Alperen Aydın <aydin.m.alperen@gmail.com>
2022-03-12 22:22:48 +0300

    Add files via upload

---
+++ HelloWorld/src/firstJavaCode.java
@@ -0,0 +1,12 @@
+
+public class firstJavaCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 10;
+		int y = 20;
+		int total = x + y;
+		System.out.println(total);
+	}
+
+}
